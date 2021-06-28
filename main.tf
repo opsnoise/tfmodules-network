5e63437539c4f2255b468d008ce50880045b7536
Abdal Garuba <abdal.garuba@gmail.com>
2021-06-27 21:08:50 -0500

    initial commit to create vpc and igw

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "aws_vpc" "this" {
+  cidr_block       = var.vpc_cidr
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.vpc_name}-igw"
+  }
+}
